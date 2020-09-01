ab77e4ce8c6faeb44a275241c647a27eadb4a0fb
rodolfolorenzo <rodolfo.olorenzo@gmail.com>
2020-09-01 14:18:33 -0300

    pseudo local library

--- 501_OD_processing_functions.R
+++ 501_OD_processing_functions.R
@@ -5,3 +5,11 @@ source("901_convert_spatial.R")
 source("902_distancia_viagem_centro.R")
 
 source("903_flag_integration.R")
+
+source("904_get_fare.R")
+
+source("905_estimate_fare_unit.R")
+
+source("906_estimate_fare.R")
+
+source("907_get_trip_fare.R")
